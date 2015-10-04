448edf0c1554cfa5a3739037ea15bd3bc6dcac50
Juan Carlos González <jcgonzalezmartin1978@htomail.com>
2015-10-04 22:33:17 +0200

    ODFB Quota Storage Update Scripts
    ODFB Quota Storage Update Scripts

---
+++ PowerShell Scripts/Office 365/PS_Set_ODFB_Storage_Quota.ps1
@@ -0,0 +1,48 @@
+﻿############################################################################################################################################
+# Script that allows to set the quota storage for a specific user ODFB.
+# Required Parameters:
+#  -> $sUserName: User Name to connect to the SharePoint Admin Center.
+#  -> $sMessage: Message to show in the user credentials prompt.
+#  -> $sSPOAdminCenterUrl: SharePoint Admin Center Url.
+#  -> $sSPOODFBHostUrl: SharePoint ODFB Host URL.
+#  -> $sSPODFBRelativePath: ODFB Relative Path.
+#  -> $iODFBQuota: Quota to be set for ODFB.
+############################################################################################################################################
+
+
+
+$host.Runspace.ThreadOptions = "ReuseThread"
+
+#Definition of the function that gets the storage space being used in ODFB for everyuser in an Office 365 tenant
+function Set-OneDriveFBQuotaStorage
+{
+    param ($sUserName,$sMessage,$sSPOAdminCenterUrl,$sSPOODFBHostUrl,$sSPODFBRelativePath,$iODFBQuota)
+    try
+    {    
+        Write-Host "----------------------------------------------------------------------------------"  -foregroundcolor Green
+        Write-Host "Setting the Quota Storage for $sUserName ODFB" -foregroundcolor Green
+        Write-Host "----------------------------------------------------------------------------------"  -foregroundcolor Green
+        $msolCred = Get-Credential -UserName $sUserName -Message $sMessage        
+        Connect-SPOService -Url $sSPOAdminCenterUrl -Credential $msolCred
+        $sODFBSite=$sSPOODFBHostUrl + $sSPODFBRelativePath        
+        Set-SPOSite –Identity $sODFBSite –StorageQuota $iODFBQuota         
+        
+        Write-Host "----------------------------------------------------------------------------------"  -foregroundcolor Green
+        Write-Host "Storage Quota sucessfully update for $sUserName ODFB" -foregroundcolor Green
+        Write-Host "----------------------------------------------------------------------------------"  -foregroundcolor Green
+    }
+    catch [System.Exception]
+    {
+        write-host -f red $_.Exception.ToString()   
+    }    
+}
+
+#Connection to Office 365
+$sUserName="<O365User>@<O365Domain>.onmicrosoft.com"
+$sMessage="Introduce your SPO Credentials"
+$sSPOAdminCenterUrl="https://<O365Domain>-admin.sharepoint.com/"
+$sSPOODFBHostUrl="https://<O365Domain>-my.sharepoint.com/personal/"
+$sSPODFBRelativePath="<O365User>_<O365Domain>_onmicrosoft_com"
+$iODFBQuota=512000
+
+Set-OneDriveFBQuotaStorage -sUserName $sUserName -sMessage $sMessage -sSPOAdminCenterUrl $sSPOAdminCenterUrl -sSPOODFBHostUrl $sSPOODFBHostUrl -sSPODFBRelativePath $sSPODFBRelativePath -iODFBQuota $iODFBQuota
---
+++ PowerShell Scripts/Office 365/PS_Set_ODFB_Storage_Quota_AllUsers.ps1
@@ -0,0 +1,55 @@
+﻿############################################################################################################################################
+# Script that allows to set the quota storage for the ODFB assigned to each user in an Office 365 tenant
+# Required Parameters:
+#  -> $sUserName: User Name to connect to the SharePoint Admin Center.
+#  -> $sMessage: Message to show in the user credentials prompt.
+#  -> $sSPOAdminCenterUrl: SharePoint Admin Center Url.
+#  -> $sSPOODFBHostUrl: SharePoint ODFB Host URL.
+#  -> $iODFBQuota: Quota to be set for ODFB.
+############################################################################################################################################
+
+$host.Runspace.ThreadOptions = "ReuseThread"
+
+#Definition of the function that sets the quota storage for all the ODFBs in an Office 365 tenant
+function Set-OneDriveFBQuotaStorage
+{
+    param ($sUserName,$sMessage,$sSPOAdminCenterUrl,$sSPOODFBHostUrl,$iODFBQuota)
+    try
+    {    
+        Write-Host "----------------------------------------------------------------------------------"  -foregroundcolor Green
+        Write-Host "Setting the Quota Storage for all the ODFBs in an Office 365 tenant" -foregroundcolor Green
+        Write-Host "----------------------------------------------------------------------------------"  -foregroundcolor Green
+        $msolCred = Get-Credential -UserName $sUserName -Message $sMessage
+        Connect-MsolService -Credential $msolCred
+        Connect-SPOService -Url $sSPOAdminCenterUrl -Credential $msolCred 
+        $spoUsers=Get-MsolUser        
+        ForEach ($spoUser in $spoUsers){
+            ForEach ($O365Plan in $spoUser.Licenses.ServiceStatus){
+                if (($O365Plan.servicePlan.servicename -like 'SharepointEnterprise') -and $O365Plan.ProvisioningStatus -eq 'Success')
+                {                           
+                    try{                        
+                        $sODFBSite=$sSPOODFBHostUrl + $spoUser.UserPrincipalName.Replace(".","_").Replace("@","_")
+                        Set-SPOSite –Identity $sODFBSite –StorageQuota $iODFBQuota
+                        Write-Host "Storage Quota updated for: $sODFBSite"  -ForegroundColor Green  
+               
+                    }catch{
+                        Write-Host "ODFB site for" + $spoUser.UserPrincipalName + " doesn't exist. Storage Quota not updated" -ForegroundColor Yellow            
+                    }
+                }
+            }
+        }
+    }
+    catch [System.Exception]
+    {
+        write-host -f red $_.Exception.ToString()   
+    }    
+}
+
+#Required parameters
+$sUserName="<Office365User>@<Office365Domain>"
+$sMessage="Introduce your SPO Credentials"
+$sSPOAdminCenterUrl="https://<Office365Domain>-admin.sharepoint.com/"
+$sSPOODFBHostUrl="https://<Office365Domain>-my.sharepoint.com/personal/"
+$iODFBQuota=512000
+
+Set-OneDriveFBQuotaStorage -sUserName $sUserName -sMessage $sMessage -sSPOAdminCenterUrl $sSPOAdminCenterUrl -sSPOODFBHostUrl $sSPOODFBHostUrl -iODFBQuota $iODFBQuota
