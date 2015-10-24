f4595686aa00f8be508186bf773491208c30de17
Juan Carlos González <jcgonzalezmartin1978@htomail.com>
2015-10-24 23:30:49 +0200

    Cloud First Camps Bogota 2015 Conference Materials
    Cloud First Camps Bogota 2015 Conference Materials

---
+++ Conference Materials/CloudFirst Camps Bogota 2015/01 Demos/PS_Get_New_Groups_Cmdlets.ps1
@@ -0,0 +1,32 @@
+﻿############################################################################################################################################
+# Script that allows to do work with Office 365 Groups. 
+# Required Parameters: N/A
+############################################################################################################################################
+
+$host.Runspace.ThreadOptions = "ReuseThread"
+
+#Definition of the function that allows to delete the Office 365 users contained in the CSV file.
+function Get-Office365GroupsCredentials
+{
+    param ($msolCred)
+    try
+    {   
+        Write-Host "-----------------------------------------------------------"  -foregroundcolor Green
+        Write-Host "Getting new Exchange Online Cmdlets." -foregroundcolor Green
+        Write-Host "-----------------------------------------------------------"  -foregroundcolor Green
+        
+        $PSSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $msolCred -Authentication Basic -AllowRedirection
+        Import-PSSession $PSSession
+
+    }
+    catch [System.Exception]
+    {
+        write-host -f red $_.Exception.ToString()   
+    }         
+}
+#Connection to Office 365
+$sUserName="<O365User>@<O365Domain>.onmicrosoft.com"
+$sMessage="Introduce your Office 365 Credentials"
+$msolCred = Get-Credential -UserName $sUserName -Message $sMessage
+
+Get-Office365GroupsCredentials -msolCred $msolCred
