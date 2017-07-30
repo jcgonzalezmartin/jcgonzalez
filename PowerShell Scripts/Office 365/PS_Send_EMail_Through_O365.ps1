8fd75f9926a32262188c2deac1d375ae32636b4d
Juan Carlos González <jcgonzalezmartin1978@hotmail.com>
2017-07-30 16:44:02 +0200

    New script added to the repository
    New PS script added to the repository

---
+++ PowerShell Scripts/Office 365/PS_Send_EMail_Through_O365.ps1
@@ -0,0 +1,52 @@
+﻿############################################################################################################################################
+# Script that allows to send an e-mail message using Office 365 SMTP Service
+# Required Parameters:
+#  ->$O365Cred: Office 365 Credentials Object
+#  ->$sFromEMail: From E-Mail
+#  ->$sToEMail: Recipients e-mails
+#  ->$sCcEMail: CC E-Mails
+#  ->$sBccEMail: Bcc E-Mails
+#  ->$sEMailSubject: E-Mail Subject
+#  ->$sEMailBody: E-Mail Body
+#  ->$sSMTPServer: SMTP Server to be used (Office 365 in this case)
+############################################################################################################################################
+
+$host.Runspace.ThreadOptions = "ReuseThread"
+
+#Definition of the function that allows to send an e-mail message using Office 365 SMTP Service
+function Send-EmailByO365
+{   
+    param($O365Cred,$sFromEMail,$sToEMail,$sCcEMail,$sBccEMail,$sEMailSubject,$sEMailBody,$sSMTPServer) 
+    Try
+    {   
+        
+    if($sToEmail -ne $null -and $sCcEMail -ne $null)
+    {
+        [string[]]$To=$sToEMail.Split(',',[System.StringSplitOptions]::RemoveEmptyEntries)
+        [string[]]$cc=$sCcEMail.Split(',',[System.StringSplitOptions]::RemoveEmptyEntries) 
+        [string[]]$bcc=$sBccEmail.Split(',',[System.StringSplitOptions]::RemoveEmptyEntries) 
+        Send-MailMessage -From $sFromEMail -To $To -Cc $cc -Bcc $sBccEMail -Subject $sEMailSubject -Body $sEMailBody -BodyAsHtml -SmtpServer $sSMTPServer -Credential $O365Cred -UseSsl 
+        }    
+    }
+    catch [System.Exception]
+    {
+        Write-Host -ForegroundColor Red $_.Exception.ToString()   
+    } 
+}
+
+#Office 365 Credentials
+$sUserName="<Your_Office365_Admin_Account>"
+$sMessage="Introduce your Office 365 Credentials"
+$O365Cred=Get-Credential -UserName $sUserName -Message $sMessage
+
+#E-Mail configuration
+$sFromEMail="<From_Email>"
+$sToEMail="<ToEMail_1>,<To_Email2>"
+$sCcEMail="<CcEMail_1>,<Cc_Email2>"
+$sBccEMail="<BccEMail_1>,<Bcc_Email2>"
+$sEMailSubject="<E-Mail Subject>"
+$sEMailBody="<E-Mail Body (just add your HTML code)>"
+$sSMTPServer="smtp.office365.com"
+
+#Sending the e-mail
+Send-EmailByO365 -O365Cred $O365Cred -sFromEMail $sFromEMail -sToEMail $sToEMail -sCcEMail $sCcEMail -sBccEMail $sBccEMail -sEMailSubject $sEMailSubject -sEMailBody $sEMailBody -sSMTPServer $sSMTPServer
