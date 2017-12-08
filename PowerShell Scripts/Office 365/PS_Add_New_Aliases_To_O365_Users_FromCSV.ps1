ce912bf68a600a1d7cb1612aab5d4e066cfe4bba
Juan Carlos González <jcgonzalezmartin1978@hotmail.com>
2017-12-08 18:14:15 +0100

    Added two new PowerShell Scripts

---
+++ PowerShell Scripts/Office 365/PS_Add_New_Aliases_To_O365_Users_FromCSV.ps1
@@ -0,0 +1,64 @@
+﻿############################################################################################################################################
+# Script that allows to do a add users to add e-mail addresses to Office 365 users in bulk using the same UPN. The users are read from a CSV file. 
+# The csv file only needs a column that stores the account principal name for each user to be added to Office 365
+# Required Parameters:
+#  -> $sUserName: User Name to connect to the SharePoint Admin Center.
+#  -> $NewDomain: Domain of the new e-mail to be added to the user.
+#  -> $sMessage: Message to show in the user credentials prompt.
+#  -> $sInputFile: Message to show in the user credentials prompt.
+############################################################################################################################################
+
+$host.Runspace.ThreadOptions = "ReuseThread"
+
+#Definition of the function that allows to add to Office 365 the list of users contained in the CSV file.
+function Add-NewEMailAddressToUsers
+{
+    param ($sInputFile,$sNewDomain)
+    try
+    {   
+        # Reading the Users CSV file
+        $bFileExists = (Test-Path $sInputFile -PathType Leaf) 
+        if ($bFileExists) { 
+            "Loading $sInputFile for processing..." 
+            $tblUsers = Import-CSV $sInputFile            
+        } else { 
+            Write-Host "$sInputFile file not found. Stopping the import process!" -ForegroundColor Red
+            exit 
+        }         
+        
+        # Deleting the users
+        Write-Host "Adding new E-Mails to Office 365 Users ..." -ForegroundColor Green    
+        foreach ($user in $tblUsers) 
+        {             
+            $sUPNPart=$user.UserPrincipalName.Split("@")[0]            
+            $sNewEMailToAdd=$sUPNPart + $sNewDomain
+             "Adding new E-Mail $sNewEMailToAdd to User " + $user.UserPrincipalName.ToString()  	
+            Set-Mailbox -Identity $user.UserPrincipalName -EmailAddresses @{add=$sNewEMailToAdd}            
+        } 
+
+        Write-Host "-----------------------------------------------------------"  -ForegroundColor Green
+        Write-Host "All the e-mails have been added. The processs is completed." -ForegroundColor Green
+        Write-Host "-----------------------------------------------------------"  -ForegroundColor Green
+    }
+    catch [System.Exception]
+    {
+        Write-Host -ForegroundColor Red $_.Exception.ToString()   
+    } 
+}
+
+#Connection to Office 365
+$sUserName="<O365_Admin>"
+$sMessage="Introduce your Office 365 Credentials"
+#Connection to Office 365
+
+
+$O365cred = Get-Credential -UserName $sUserName -Message $sMessage
+$PSSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $O365cred -Authentication Basic -AllowRedirection
+Import-PSSession $PSSession
+
+$ScriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
+#$sInputFile=$ScriptDir+ "\O365UsersToUpdate.csv"
+
+$sNewDomain="@<YourDomain>.onmicrosoft.com"
+
+Add-NewEMailAddressToUsers -sInputFile $sInputFile -sNewDomain $sNewDomain
---
+++ PowerShell Scripts/Office 365/PS_Change_Password_To_Office_365_Users_In_Bulk.ps1
@@ -0,0 +1,60 @@
+﻿############################################################################################################################################
+# Script that allows to set a default password to serveral users in your Office 365 tenants. The users are read from a CSV file. 
+# The csv file only needs a column that stores the account principal name for each user to be added to Office 365.
+# Required Parameters:
+#  -> $sUserName: User Name to connect to the SharePoint Admin Center.
+#  -> $sDefaultPassword: Common password to be set for all the users in the CSV file.
+#  -> $sMessage: Message to show in the user credentials prompt.
+#  -> $sInputFile: Message to show in the user credentials prompt.
+############################################################################################################################################
+
+$host.Runspace.ThreadOptions = "ReuseThread"
+
+#Definition of the function that allows to add to Office 365 the list of users contained in the CSV file.
+function Set-PasswordToUsers
+{
+    param ($sInputFile,$sDefaultPassword)
+    try
+    {   
+        # Reading the Users CSV file
+        $bFileExists = (Test-Path $sInputFile -PathType Leaf) 
+        if ($bFileExists) { 
+            "Loading $sInputFile for processing..." 
+            $tblUsers = Import-CSV $sInputFile            
+        } else { 
+            Write-Host "$sInputFile file not found. Stopping the import process!" -ForegroundColor Red
+            exit 
+        }         
+        
+        # Deleting the users
+        Write-Host "Changing password for Office 365 user..." -ForegroundColor Green    
+        foreach ($user in $tblUsers) 
+        {   
+            Write-Host "Changing password for user " $user.UserPrincipalName.ToString()  	          
+            Set-MsolUserPassword -UserPrincipalName $user.UserPrincipalName -NewPassword $sDefaultPassword -ForceChangePassword $true           
+        } 
+
+        Write-Host "-----------------------------------------------------------"  -ForegroundColor Green
+        Write-Host "All passwords have been updated. The processs is completed." -ForegroundColor Green
+        Write-Host "-----------------------------------------------------------"  -ForegroundColor Green
+    }
+    catch [System.Exception]
+    {
+        Write-Host -ForegroundColor Red $_.Exception.ToString()   
+    } 
+}
+
+#Connection to Office 365
+$sUserName="<O365_Admin>
+$sMessage="Introduce your Office 365 Credentials"
+#Connection to Office 365
+$O365Cred = Get-Credential -UserName $sUserName -Message $sMessage
+
+Connect-MsolService -Credential $O365Cred
+
+$ScriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
+$sInputFile=$ScriptDir+ "\O365UsersToUpdate.csv"
+
+$sDefaultPassword="<Default_Password>"
+
+Set-PasswordToUsers -sInputFile $sInputFile -sDefaultPassword $sDefaultPassword
