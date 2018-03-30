8afd431d0534e89e236fea6c14220285ff4dc601
Juan Carlos González <jcgonzalezmartin1978@hotmail.com>
2018-03-30 17:16:05 +0200

    2 new scripts added to the repository
    2 new scripts added to the repository:
    (1) How to deploy a new SPO theme
    (2) How to get all e-mail forwardings in an Office 365 tenant

---
+++ PowerShell Scripts/Office 365/PS_Deploy_New_SPO_Theme.ps1
@@ -0,0 +1,72 @@
+﻿############################################################################################################################################
+# Script that allows to deploy a new theme to a SharePoint Online tenant
+# Required Parameters:
+#  -> $sThemeToDeploy: Theme to be deployed in the tenant.
+#  -> $sSPOThemeName: Theme Name.
+############################################################################################################################################
+
+$host.Runspace.ThreadOptions = "ReuseThread"
+if ((Get-Module Microsoft.Online.SharePoint.PowerShell).Count -eq 0) {
+    Import-Module Microsoft.Online.SharePoint.PowerShell -DisableNameChecking
+    }
+
+#Definition of the function that remove external users in a SharePoint Online Tenant.
+function Deploy-SPOTheme
+{
+    param ($sThemeToDeploy,$sSPOThemeName)
+    try
+    {  
+        Add-SPOTheme -Name $sSPOThemeName -Palette $sThemeToDeploy -IsInverted $false -Overwrite
+
+        Write-Host "----------------------------------------------------------------------------"  -ForegroundColor Green
+        Write-Host "Theme Deploying. Getting the list of Themes in the SPO Tenant" -ForegroundColor Green
+        Write-Host "----------------------------------------------------------------------------"  -ForegroundColor Green
+        Get-SPOTheme
+    }
+    catch [System.Exception]
+    {
+        Write-Host -ForegroundColor red $_.Exception.ToString()   
+    }    
+}
+
+#Connection to Office 365
+$sUserName="<spo_user>@<O365Domain>.onmicrosoft.com"
+$sMessage="Introduce your SPO Credentials"
+$sSPOAdminCenterUrl="https://<O365Domain>-admin.sharepoint.com/"
+$sThemeToDeploy=@{
+"themePrimary" = "#6a9e8b";
+"themeLighterAlt" = "#070b0a";
+"themeLighter" = "#131d19";
+"themeLight" = "#2d463d";
+"themeTertiary" = "#4e7869";
+"themeSecondary" = "#639985";
+"themeDarkAlt" = "#79a897";
+"themeDark" = "#a0c1b5";
+"themeDarker" = "#adcabf";
+"neutralLighterAlt" = "#3f3b3a";
+"neutralLighter" = "#474341";
+"neutralLight" = "#544f4e";
+"neutralQuaternaryAlt" = "#5d5755";
+"neutralQuaternary" = "#635d5b";
+"neutralTertiaryAlt" = "#807875";
+"neutralTertiary" = "#631a0a";
+"neutralSecondary" = "#aa2d11";
+"neutralPrimaryAlt" = "#d93916";
+"neutralPrimary" = "#e43c17";
+"neutralDark" = "#f08168";
+"black" = "#f2927d";
+"white" = "#353231";
+"primaryBackground" = "#353231";
+"primaryText" = "#e43c17";
+"bodyBackground" = "#353231";
+"bodyText" = "#e43c17";
+"disabledBackground" = "#474341";
+"disabledText" = "#807875";
+}
+$sSPOThemeName="New Corporate SPO Theme (2)"
+
+#Connecting to SPO
+$Office365Cred = Get-Credential -UserName $sUserName -Message $sMessage
+Connect-SPOService -Url $sSPOAdminCenterUrl -Credential $Office365Cred 
+
+Deploy-SPOTheme -sSPOThemeName $sSPOThemeName -sThemeToDeploy $sThemeToDeploy
---
+++ PowerShell Scripts/Office 365/PS_Get_Email_Forwardings_Office365.ps1
@@ -0,0 +1,48 @@
+﻿############################################################################################################################################
+# Script that allows to do get a e-mail forwarding configuration for a user/shared mailbox or all the e-mail forwardings for all the user mailboxes and
+# shared Mailboxes in a tenant.
+# Required Parameters: 
+#    -> $sOperationType
+#    -> $sUserName
+############################################################################################################################################
+
+#Definition of the function that allows to do get a e-mail forwarding configuration for a user/shared mailbox or all the e-mail forwardings for all the user mailboxes and Shared Mailboxes in a tenant.
+function Get-ConfiguredMailForwarding
+{
+    param ($sOperationType,$sUserName)       
+    try
+    {
+        switch ($sOperationType) 
+        { 
+        "SingleMailBox" {
+            Write-Host "Get e-mail forwarding configuration for $sUserName" -ForegroundColor Green                        
+            Get-Mailbox -Identity $sUserName | Format-Table DisplayName, ForwardingAddress, ForwardingSmtpAddress, DeliverToMailboxAndForward
+            } 
+        "All" {
+            Write-Host "Get-email forwarding configuration for all the user/shared mailboxes in a tenant" -ForegroundColor Green                 
+            Get-Mailbox -RecipientTypeDetails UserMailbox, SharedMailbox | ? {$_.ForwardingAddress –ne $Null -or $_.ForwardingSmtpAddress -ne $Null} | Format-Table DisplayName, ForwardingAddress, ForwardingSmtpAddress, DeliverToMailboxAndForward 
+            }        
+        default {
+            Write-Host "Requested Operation not valid!!" -ForegroundColor DarkBlue            
+            }
+        }
+    }
+    catch [System.Exception]
+    {
+        Write-Host -ForegroundColor red $_.Exception.ToString()
+    }
+}
+$sUserName="jcgonzalez@nuberosnet.onmicrosoft.com"
+$sMessage="Introduce your Office 365 Credentials"
+$O365Cred = Get-Credential -UserName $sUserName -Message $sMessage
+$EXOSession= New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $O365Cred -Authentication Basic -AllowRedirection
+Import-PSSession $EXOSession
+
+#Single User/Shared MailBox
+$sOperationType="SingleMailBox"
+Get-ConfiguredMailForwarding -sOperationType $sOperationType -sUserName $sUserName
+
+#All Users/Shared MailBoxes
+$sOperationType="All"
+Get-ConfiguredMailForwarding -sOperationType $sOperationType
+
