0bb5189f6d5bcf9d28357d216e58d9f7aba04111
Juan Carlos González <jcgonzalezmartin1978@hotmail.com>
2026-02-24 17:37:08 +0100

    Delete Conference Materials/Global Power Platform Bootcamp 2026/Overview.cmd

--- Conference Materials/Global Power Platform Bootcamp 2026/Overview.cmd
+++
@@ -1,9 +0,0 @@
-## Materiales del Global Power Platform Bootcamp
-
-Los materiales disponibles del evento **Global Power Platform Bootcamp** son los siguientes:
-
-- **Slides de las presentaciones** realizadas sobre:
-  - *Lleva tus Sitios de SharePoint al siguiente nivel: Agentes de Conocimiento en Sitios de SharePoint*
-  - *Del Low Code al Smart Code: Cómo la IA y Power Platform están democratizando el desarrollo*
-
-
